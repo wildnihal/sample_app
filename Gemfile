ff06a0393259ba584683dc750eac1cc31919a74b
Nihaal Dhakal <nihaldhakal41@gmail.com>
2019-09-10 10:03:21 +0545

    Implemented basic login

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
+# gem jquery
+
+gem 'jquery-rails'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
@@ -44,6 +47,8 @@ group :development, :test do
   gem 'capybara', '>= 2.15'
   gem 'selenium-webdriver'
   gem 'rails-controller-testing'
+  gem 'pry'
+  gem 'pry-rails'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get 'contact' =>'static_pages#contact'
   get 'signup' => 'users#new'
   post 'signup' => 'users#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
